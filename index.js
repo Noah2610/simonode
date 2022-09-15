b943b01d04053aeb20a8d1818ae2e740c75242cc
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-09-15 22:32:28 +0200

    Add color output with chalk package

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const prompt = require("prompt-sync")({ sigint: true });
+import chalk from "chalk";
+import promptSync from "prompt-sync";
 
-const COLORS = ["red", "green", "blue", "yellow"];
+const prompt = promptSync({ sigint: true });
+
+const Color = {
+    red: "red",
+    green: "green",
+    blue: "blue",
+    yellow: "yellow",
+};
+
+const COLORS = [
+    Color.red,
+    Color.green,
+    Color.blue,
+    Color.yellow,
+];
 
 function genColor() {
     return COLORS[
@@ -25,9 +40,24 @@ function isTargetColor(targetColor, compareColor) {
     return targetColor.startsWith(compareColor);
 }
 
+function displayColor(color) {
+    return chalk[color](color.toUpperCase());
+}
+
 function main() {
-    const score = run();
-    console.log(`Game Over!\nScore: ${score}`);
+    const result = run();
+    gameOver(result);
+}
+
+function gameOver({ score, pattern }) {
+    console.log(
+        [
+            chalk.bold.bgRed.black("Game Over!"),
+            `Score: ${chalk.bold.bgGreen.black(score)}`,
+            "Pattern:",
+            "  " + pattern.map(displayColor).join("\n  "),
+        ].join("\n"),
+    );
 }
 
 function run() {
@@ -38,10 +68,12 @@ function run() {
         pattern.push(color);
 
         console.clear();
-        console.log(`New color: ${color}`);
+        console.log(`New color: ${displayColor(color)}`);
 
         for (let i = 0; i < pattern.length; ) {
-            const inputColors = promptGuesses(`${i + 1}. `);
+            const inputColors = promptGuesses(
+                chalk.bold(`${i + 1}. `),
+            );
 
             for (
                 let j = 0;
@@ -54,7 +86,10 @@ function run() {
                 if (
                     !isTargetColor(targetColor, inputColor)
                 ) {
-                    return pattern.length - 1;
+                    return {
+                        score: pattern.length - 1,
+                        pattern,
+                    };
                 }
             }
         }
