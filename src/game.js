36869264b962b889247bc631bea338acf7e91269
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2022-09-15 23:41:39 +0200

    Refactor code into modules

--- src/game.js
+++ src/game.js
@@ -1,10 +1,14 @@
 import chalk from "chalk";
-import fs from "fs";
 import promptSync from "prompt-sync";
-
-const META = getMeta();
+import { getMeta } from "./util";
 
 const prompt = promptSync({ sigint: true });
+
+export function play() {
+    printInstructions();
+    const result = run();
+    gameOver(result);
+}
 
 const Color = {
     red: "red",
@@ -19,108 +23,6 @@ const COLORS = [
     Color.blue,
     Color.yellow,
 ];
-
-function main() {
-    printInstructions();
-    const result = run();
-    gameOver(result);
-}
-
-function getMeta() {
-    const file = new URL("./package.json", import.meta.url)
-        .pathname;
-
-    try {
-        const raw = fs.readFileSync(file, {
-            encoding: "utf8",
-        });
-        const obj = JSON.parse(raw);
-        return {
-            name: obj.name,
-            version: obj.version,
-            author:
-                (obj.author.split("<")[0] || "").trim() ||
-                undefined,
-        };
-    } catch (e) {
-        err(
-            `Failed reading file ${chalk.bold(
-                file,
-            )}\n${JSON.stringify(e, null, 2)}`,
-        );
-    }
-}
-
-function err(msg) {
-    console.error(`${chalk.bold.red("[Error]")}\n${msg}`);
-    process.exit(1);
-}
-
-function genColor() {
-    return COLORS[
-        Math.floor(Math.random() * COLORS.length)
-    ];
-}
-
-function promptGuesses(msg = "> ") {
-    return prompt(msg)
-        .toLowerCase()
-        .trim()
-        .split("") // /,|\s+/
-        .map((s) => s.trim())
-        .filter(isValidColor);
-}
-
-function isValidColor(s) {
-    return s && COLORS.some((c) => isTargetColor(c, s));
-}
-
-function isTargetColor(targetColor, compareColor) {
-    return targetColor.startsWith(compareColor);
-}
-
-function displayColor(color) {
-    return chalk[color](color.toUpperCase());
-}
-
-function printInstructions() {
-    prompt(
-        [
-            chalk.bgWhite.black.bold(META.name) +
-                chalk.bold(` v${META.version}`) +
-                " by " +
-                chalk.bold(META.author),
-            "Play Simon in your console!",
-            `Each round, you will be given a ${chalk.bold(
-                "color",
-            )} to memorize.`,
-            "You will have to enter the full pattern of all colors to keep playing.",
-            "You lose when you input a wrong color.",
-            "To input colors, simply type the letters corresponding to these colors:",
-            "  " +
-                COLORS.map(
-                    (c) =>
-                        `${chalk.bold(
-                            c[0],
-                        )} - ${displayColor(c)}`,
-                ).join("\n  "),
-            "Press ENTER to play!\n",
-        ].join("\n"),
-        { echo: "" },
-    );
-}
-
-function gameOver({ score, pattern }) {
-    console.clear();
-    console.log(
-        [
-            chalk.bold.bgRed.black("Game Over!"),
-            `Score: ${chalk.bold.bgGreen.black(score)}`,
-            "Pattern:",
-            "  " + pattern.map(displayColor).join("\n  "),
-        ].join("\n"),
-    );
-}
 
 function run() {
     const pattern = [];
@@ -162,4 +64,69 @@ function run() {
     }
 }
 
-main();
+function genColor() {
+    return COLORS[
+        Math.floor(Math.random() * COLORS.length)
+    ];
+}
+
+function promptGuesses(msg = "> ") {
+    return prompt(msg)
+        .toLowerCase()
+        .trim()
+        .split("") // /,|\s+/
+        .map((s) => s.trim())
+        .filter(isValidColor);
+}
+
+function isValidColor(s) {
+    return s && COLORS.some((c) => isTargetColor(c, s));
+}
+
+function isTargetColor(targetColor, compareColor) {
+    return targetColor.startsWith(compareColor);
+}
+
+function displayColor(color) {
+    return chalk[color](color.toUpperCase());
+}
+
+function printInstructions() {
+    const meta = getMeta();
+    prompt(
+        [
+            chalk.bgWhite.black.bold(meta.name) +
+                chalk.bold(` v${meta.version}`) +
+                " by " +
+                chalk.bold(meta.author),
+            "Play Simon in your console!",
+            `Each round, you will be given a ${chalk.bold(
+                "color",
+            )} to memorize.`,
+            "You will have to enter the full pattern of all colors to keep playing.",
+            "You lose when you input a wrong color.",
+            "To input colors, simply type the letters corresponding to these colors:",
+            "  " +
+                COLORS.map(
+                    (c) =>
+                        `${chalk.bold(
+                            c[0],
+                        )} - ${displayColor(c)}`,
+                ).join("\n  "),
+            "Press ENTER to play!\n",
+        ].join("\n"),
+        { echo: "" },
+    );
+}
+
+function gameOver({ score, pattern }) {
+    console.clear();
+    console.log(
+        [
+            chalk.bold.bgRed.black("Game Over!"),
+            `Score: ${chalk.bold.bgGreen.black(score)}`,
+            "Pattern:",
+            "  " + pattern.map(displayColor).join("\n  "),
+        ].join("\n"),
+    );
+}
---
+++ src/util.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+
+export function err(msg) {
+    console.error(`${chalk.bold.red("[Error]")}\n${msg}`);
+    process.exit(1);
+}
+
+let META = null;
+
+export function getMeta() {
+    return META || (META = readMeta());
+}
+
+function readMeta() {
+    const file = new URL("./package.json", import.meta.url)
+        .pathname;
+
+    try {
+        const raw = fs.readFileSync(file, {
+            encoding: "utf8",
+        });
+        const obj = JSON.parse(raw);
+        return {
+            name: obj.name,
+            version: obj.version,
+            author:
+                (obj.author.split("<")[0] || "").trim() ||
+                undefined,
+        };
+    } catch (e) {
+        err(
+            `Failed reading file ${chalk.bold(
+                file,
+            )}\n${JSON.stringify(e, null, 2)}`,
+        );
+    }
+}
